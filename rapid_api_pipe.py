3ad9282403b5e666c9e5fa6341bc590ac5a07587
bkiruthu <65987908+bkiruthu@users.noreply.github.com>
2022-02-02 12:32:39 -0800

    Update rapid_api_pipe.py

--- rapid_api_pipe.py
+++ rapid_api_pipe.py
@@ -6,42 +6,40 @@ import datetime
 import pytz
 
 
-
 def rapid_api():
     path = "https://api.covid19api.com/live/country/usa/status/confirmed/date/"
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(days=1)
-    year = yesterday.strftime("%Y")
-    month = yesterday.strftime("%m")
-    day = yesterday.strftime("%d")
-    url = path + str(yesterday)
-
+    url = path + str(yesterday.strftime("%Y-%m-%d"))
+    print(url)
     payload = {}
     headers = {}
-    
-    response = requests.request("GET", url, headers=headers, data=payload)
-    rawdata=response.json
+    rawdata = requests.get(url).json()
     return rawdata
 
 # handler for lambda
+
+
 def covid(event, context):
-    data_output=rapid_api()
+    data_output = rapid_api()
     tz = pytz.timezone("US/Eastern")
     timestamp = datetime.datetime.now(tz).strftime("%m_%d_%y_%H%M")
     date = datetime.datetime.now(tz).strftime("%Y/%m/%d")
-    file_name =f"{date}/covid-{timestamp}.json"
+    file_name = f"{date}/covid-{timestamp}.json"
     print(file_name)
     bucket_name = 'rapidapi-covid'
     save_file_to_s3(bucket_name, file_name, data_output)
     timestamp = None
 
 # save to a bucket
+
+
 def save_file_to_s3(bucket, file_name, data_output):
-   s3 = boto3.resource('s3')
-   obj = s3.Object(bucket, file_name)
-   tmp_file_path = "/tmp/nyrawdata.json"
-   with open(tmp_file_path, "w") as file:
-       file.write(str(data_output))
-    
-   s3.Bucket(bucket).upload_file(tmp_file_path, file_name)
-   print('file added to s3')
+    s3 = boto3.resource('s3')
+    obj = s3.Object(bucket, file_name)
+    tmp_file_path = "/tmp/nyrawdata.json"
+    with open(tmp_file_path, "w") as file:
+        file.write(str(data_output))
+
+    s3.Bucket(bucket).upload_file(tmp_file_path, file_name)
+    print('file added to s3')
