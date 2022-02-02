ec875dde51a2105db8b6866b625e8e4342091c78
bkiruthu <65987908+bkiruthu@users.noreply.github.com>
2022-02-01 17:13:34 -0800

    Initial

---
+++ rapid_api_pipe.py
@@ -0,0 +1,46 @@
+import requests
+import boto3
+import time
+import json
+import datetime
+import pytz
+
+
+def rapid_api():
+    path = "https://api.covid19api.com/live/country/usa/status/confirmed/date/"
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    year = yesterday.strftime("%Y")
+    month = yesterday.strftime("%m")
+    day = yesterday.strftime("%d")
+
+    url = path + str(yesterday)
+
+    payload = {}
+    headers = {}
+    
+    response = requests.request("GET", url, headers=headers, data=payload)
+    rawdata=response.json
+    return rawdata
+
+
+# handler for lambda
+def covid(event, context):
+    data_output=rapid_api()
+    tz = pytz.timezone("US/Eastern")
+    timestamp = datetime.datetime.now(tz).strftime("%m_%d_%y_%H%M")
+    file_name =f"covid-{timestamp}.json"
+    print(file_name)
+    save_file_to_s3('cdc-ohio-covid', file_name, data_output)
+    timestamp = None
+
+# save to a bucket
+def save_file_to_s3(bucket, file_name, data_output):
+   s3 = boto3.resource('s3')
+   obj = s3.Object(bucket, file_name)
+   tmp_file_path = "/tmp/nyrawdata.json"
+   with open(tmp_file_path, "w") as file:
+       file.write(data_output)
+    
+   s3.Bucket(bucket).upload_file(tmp_file_path, file_name)
+   print('file added to s3')
